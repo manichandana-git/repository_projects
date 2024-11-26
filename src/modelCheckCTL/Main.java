1c42f3c5719dac773e869844f84b8e3d60fbc61f
Sravani-Baddi <nagasravanibaddims@gmail.com>
2024-11-26 15:40:28 -0600

    testing commit

--- src/modelCheckCTL/Main.java
+++ src/modelCheckCTL/Main.java
@@ -2,9 +2,12 @@ package modelCheckCTL;
 
 import modelCheckCTL.controller.ModelCheckerController;
 import modelCheckCTL.view.ModelCheckerView;
+import android.util.Log;  // Import the Log class
+
 
 public class Main {
     public static void main(String[] args) {
+        Log.e("MAIN", "NewCommit Testing");
         javax.swing.SwingUtilities.invokeLater(() -> {
             ModelCheckerView view = new ModelCheckerView();
             new ModelCheckerController(view);
